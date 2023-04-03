5e223e06483e92830b96aebbb3ca6d5d8fbfc7cf
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-03 15:53:55 +0530

    loading skelton ui updated

--- frontend/src/components/CardLoadingSkelton.tsx
+++ frontend/src/components/CardLoadingSkelton.tsx
@@ -1,12 +1,7 @@
 const CardLoadingSkelton = () => {
   return (
-    <section className="bg-white w-full">
+    <section className="bg-white w-full sticky top-0 z-50">
       <div className="container px-6 py-10 mx-auto animate-pulse">
-        <h1 className="w-48 h-2 mx-auto bg-gray-200 rounded-lg"></h1>
-
-        <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg"></p>
-        <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg sm:w-80"></p>
-
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
           <div className="w-full ">
             <div className="w-full h-64 bg-gray-300 rounded-lg"></div>
