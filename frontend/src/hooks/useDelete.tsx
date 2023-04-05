801cc047d3ffc02982ba584a2242fba89d2778f6
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-05 14:54:59 +0530

    toast added

--- frontend/src/hooks/useDelete.tsx
+++ frontend/src/hooks/useDelete.tsx
@@ -1,18 +1,31 @@
 import axios from "axios";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 const useDelete = <T,>({ url }: { url: string }) => {
   const [deleteResponse, setDeleteResponse] = useState<
     Promise<unknown> | any
   >();
   const [deleteErr, setDeleteErr] = useState<boolean>(false);
-  const deleteOnAction = async (asin: string) => {
+  const deleteOnAction = async (
+    asin: string,
+    success: string,
+    pending: string,
+    error: string
+  ) => {
     try {
-      const { data: res } = await axios.delete(`${url}/${asin}`, {
-        headers: {
-          Authorization: sessionStorage.getItem("token"),
-        },
-      });
+      const { data: res } = await toast.promise(
+        axios.delete(`${url}/${asin}`, {
+          headers: {
+            Authorization: sessionStorage.getItem("token"),
+          },
+        }),
+        {
+          success,
+          pending,
+          error,
+        }
+      );
       setDeleteResponse(await res);
     } catch (error) {
       setDeleteErr(true);
