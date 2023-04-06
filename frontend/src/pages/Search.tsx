bd314b178148d8f8c462561a88e7bec5398b0ddb
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-06 14:31:54 +0530

    fetchOnAction added in search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -9,11 +9,12 @@ const Search: React.FC = () => {
   const [searchList, setSearchList] = useState<IProduct[] | null | undefined>();
   const params = useParams();
   const baseURI = import.meta.env.VITE_API_BASE_URI;
-  const { response, fetchErr, loading } = useFetch<IProduct[]>({
+  const { response, fetchErr, loading, FetchOnAction } = useFetch<IProduct[]>({
     url: `${baseURI}/filter/search/?q=${params.query}`,
   });
   useEffect(() => {
-    setSearchList(response);
+    FetchOnAction();
+    response && setSearchList(response);
   }, [response, params]);
 
   if (fetchErr) return <p>Fetch Error occur</p>;
@@ -21,7 +22,11 @@ const Search: React.FC = () => {
   return (
     <>
       {loading && <Loader message="searching..." />}
-      {searchList?.length === 0 && <div className="container mx-auto text-center text-2xl text-indigo-500">No Item Found!!</div>}
+      {searchList?.length === 0 && (
+        <div className="container mx-auto text-center text-2xl text-indigo-500">
+          No Item Found!!
+        </div>
+      )}
       <div className="container mx-auto flex w-full justify-center flex-wrap">
         {searchList?.map((list) => (
           <Card product={list} key={list.asin} grow={0.2} />
