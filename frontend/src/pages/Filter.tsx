9e4358a0eed43f9d9e4ff8d610442c1ef482d08b
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-11 15:47:55 +0530

    navigate to filters page with query

--- frontend/src/pages/Filter.tsx
+++ frontend/src/pages/Filter.tsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import IProduct from "../interfaces/product";
+import Card from "../components/Card";
 
 const Filter = () => {
-  const { params } = useParams();
-  console.log(params);
+  const params = useParams();
+  const [products, setProducts] = useState<IProduct[]>();
+  const baseURI = import.meta.env.VITE_API_BASE_URI;
+  const { FetchOnAction, fetchErr, loading, response } = useFetch<IProduct[]>({
+    url: `${baseURI}/filter/${params?.query?.split("&&")[1]}/?${
+      params?.query?.split("&&")[0]
+    }`,
+  });
+
+  useEffect(() => {
+    FetchOnAction();
+  }, []);
+
+  useEffect(() => {
+    response && setProducts(response);
+  }, [response]);
+
+  if (fetchErr) return <h1>Fetch Error...</h1>;
+
   return (
     <>
-      <h1>Hello world</h1>
+      <div className="flex container flex-wrap py-8">
+        {products &&
+          products.map((product, index) => (
+            <Card
+              product={product}
+              grow={index + 1 === products.length ? 0.1 : 1}
+              key={product.asin}
+            />
+          ))}
+      </div>
     </>
   );
 };
