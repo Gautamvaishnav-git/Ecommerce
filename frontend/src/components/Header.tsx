369d97513824a5d72eef85cfaf2e13a7aff0b0d3
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-12 15:52:52 +0530

    added logo in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import { AiFillHome, AiOutlineShoppingCart } from "react-icons/ai";
+import freezoneLogo from "../assets/freezoneicon.png";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
@@ -25,21 +26,9 @@ const Header: React.FC = () => {
         <div className="container mx-auto flex flex-wrap p-5 justify-between items-center">
           <Link
             to="/"
-            className="flex title-font font-medium items-center text-gray-900 dark:text-white mb-4 md:mb-0"
+            className="flex gap-1 title-font font-medium items-center text-gray-900 dark:text-white mb-4 md:mb-0"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">Free Zone</span>
+            <img src={freezoneLogo} alt="logo" className="w-44" />
           </Link>
           <form
             onSubmit={handleSearch}
@@ -63,10 +52,16 @@ const Header: React.FC = () => {
             className="md:ml-auto flex flex-wrap items-center text-base justify-center"
             id="navBar"
           >
-            <NavLink to="/" className="mr-5 hover:text-gray-900 dark:hover:text-white duration-200">
+            <NavLink
+              to="/"
+              className="mr-5 hover:text-gray-900 dark:hover:text-white duration-200"
+            >
               Home
             </NavLink>
-            <NavLink to="/cart" className="mr-5 hover:text-gray-900 dark:hover:text-white duration-200">
+            <NavLink
+              to="/cart"
+              className="mr-5 hover:text-gray-900 dark:hover:text-white duration-200"
+            >
               Cart
             </NavLink>
           </nav>
@@ -97,31 +92,19 @@ const Header: React.FC = () => {
 export const MobileMenu: React.FC = () => {
   return (
     <>
-      <nav className="flex justify-between px-3 pt-2 bg-white/60 dark:bg-gray-800/60 backdrop-blur-sm">
+      <nav className="flex justify-between items-center px-3 pt-2 bg-white/60 dark:bg-gray-800/60 backdrop-blur-sm">
         <Link
           to="/"
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">Free Zone</span>
+         <img src={freezoneLogo} alt="logo" className="w-28" />
         </Link>
-        <div className="flex gap-4">
+        <div className="flex gap-4 mb-3">
           <Link to="/">
-            <AiFillHome className="text-indigo-600 rounded-full p-2 w-8 h-8 bg-slate-200/50 text-2xl" />
+            <AiFillHome className="text-indigo-600 rounded-full p-2 w-8 h-8 bg-slate-200/50 dark:bg-gray-600/60 dark:text-white text-2xl" />
           </Link>
           <Link to="/cart">
-            <AiOutlineShoppingCart className="text-indigo-600 rounded-full p-2 w-8 h-8 bg-slate-200/50 text-2xl" />
+            <AiOutlineShoppingCart className="text-indigo-600 rounded-full p-2 w-8 h-8 bg-slate-200/50 dark:bg-gray-600/60 dark:text-white text-2xl" />
           </Link>
         </div>
       </nav>
