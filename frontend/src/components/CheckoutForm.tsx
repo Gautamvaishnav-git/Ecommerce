ac4b4611d957b6ca8a15ec7e2e8a57c69df61aa8
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-20 17:26:46 +0530

    checkout api intigrated

---
+++ frontend/src/components/CheckoutForm.tsx
@@ -0,0 +1,163 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import usePost from "../hooks/usePost";
+import ServerErr from "./ServerErr";
+import { ToastContainer } from "react-toastify";
+
+interface formData {
+  street: string;
+  city: string;
+  state: string;
+  pincode: string;
+}
+
+const CheckoutForm = (props: PropTypes) => {
+  const { asins, back, setBack } = props;
+  const baseURI = import.meta.env.VITE_API_BASE_URI;
+  const [details, setDetails] = useState<formData>({
+    street: "",
+    state: "",
+    pincode: "",
+    city: "",
+  });
+
+  const { postOnAction, postErr } = usePost({
+    url: `${baseURI}/checkout`,
+    data: { items: asins, address: details },
+  });
+
+  const inputChangeEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setDetails({ ...details, [name]: value });
+  };
+
+  const placeOrder = (e: React.SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
+    postOnAction("order placed", "not placed due to err", "wait a moment");
+    console.log(details);
+  };
+
+  if (postErr) return <ServerErr />;
+  return (
+    <div>
+      <ToastContainer />
+      <div className="py-2">Back</div>
+      <div className="flex items-center justify-center p-12 fixed top-0 h-screen w-full mx-auto bg-slate-900 z-10 left-0">
+        <div className="mx-auto w-full max-w-[550px]">
+          <div className="pb-8 text-white flex items-center">
+            <button
+              className="flex gap-2 hover:gap-3 duration-150 items-center"
+              onClick={() => setBack(!back)}
+            >
+              <AiOutlineArrowLeft className="inline" />
+              <span>Back</span>
+            </button>
+          </div>
+          <form onSubmit={placeOrder}>
+            <div className="flex flex-wrap w-full gap-4">
+              <div className="w-full sm:w-fit">
+                <div className="mb-5">
+                  <label
+                    htmlFor="street"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    Street
+                  </label>
+                  <input
+                    onChange={inputChangeEvent}
+                    value={details?.street}
+                    required
+                    type="text"
+                    name="street"
+                    id="street"
+                    placeholder="street"
+                    className="w-full rounded-md border border-gray-600 duration-200 bg-gray-800 py-3 px-6 text-base font-medium text-gray-200 outline-none focus:border-indigo-500 focus:shadow-md"
+                  />
+                </div>
+              </div>
+              <div className="w-full sm:w-fit">
+                <div className="mb-5">
+                  <label
+                    htmlFor="city"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    City
+                  </label>
+                  <input
+                    onChange={inputChangeEvent}
+                    value={details?.city}
+                    required
+                    type="text"
+                    name="city"
+                    id="city"
+                    placeholder="City ex- mumbai"
+                    className="w-full rounded-md border border-gray-600 duration-200 bg-gray-800 py-3 px-6 text-base font-medium text-gray-200 outline-none focus:border-indigo-500 focus:shadow-md"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-wrap w-full gap-4">
+              <div className="w-full sm:w-fit">
+                <div className="mb-5">
+                  <label
+                    htmlFor="state"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    State
+                  </label>
+                  <input
+                    onChange={inputChangeEvent}
+                    value={details?.state}
+                    required
+                    type="text"
+                    name="state"
+                    id="state"
+                    placeholder="state"
+                    className="w-full rounded-md border border-gray-600 duration-200 bg-gray-800 py-3 px-6 text-base font-medium text-gray-200 outline-none focus:border-indigo-500 focus:shadow-md"
+                  />
+                </div>
+              </div>
+              <div className="w-full sm:w-fit">
+                <div className="mb-5">
+                  <label
+                    htmlFor="pincode"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    pincode
+                  </label>
+                  <input
+                    onChange={inputChangeEvent}
+                    value={details?.pincode}
+                    required
+                    type="number"
+                    name="pincode"
+                    id="pincode"
+                    placeholder="pincode ex- 23091"
+                    className="w-full rounded-md border border-gray-600 duration-200 bg-gray-800 py-3 px-6 text-base font-medium text-gray-200 outline-none focus:border-indigo-500 focus:shadow-md appearance-none"
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <button
+                className="rounded-md bg-indigo-500 py-3 px-8 text-center text-base font-semibold text-white outline-none border border-transparent hover:border-indigo-500 hover:bg-transparent duration-200"
+                type="submit"
+              >
+                Order
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutForm;
+
+type PropTypes = {
+  asins: string[];
+  back: boolean;
+  setBack: Dispatch<SetStateAction<boolean>>;
+};
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -5,6 +5,9 @@ import useFetch from "../hooks/useFetch";
 import IProduct from "../interfaces/product";
 import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
+import { MdShoppingCartCheckout } from "react-icons/md";
+import ServerErr from "../components/ServerErr";
+import CheckoutForm from "../components/CheckoutForm";
 
 export interface cartProductType extends IProduct {
   quantity: number;
@@ -13,6 +16,9 @@ export interface cartProductType extends IProduct {
 const Cart = () => {
   const baseURI = import.meta.env.VITE_API_BASE_URI;
   const [fetchURI, setFetchURI] = useState(`${baseURI}/cart/fetchcart`);
+  const [showCheckoutForm, setShowCheckoutForm] = useState(false);
+  const [asins, setAsins] = useState<string[]>([]);
+
   const { response, fetchErr, loading, FetchOnAction } = useFetch<
     cartProductType[]
   >({
@@ -22,6 +28,14 @@ const Cart = () => {
   const { deleteResponse, deleteOnAction } = useDelete({
     url: `${baseURI}/cart/delete`,
   });
+
+  const checkoutCart = () => {
+    if (response) {
+      response.map((item) => setAsins((prev) => [...prev, item.asin]));
+      setShowCheckoutForm(!showCheckoutForm);
+    }
+  };
+
   const deleteProduct = (asin: string) => {
     deleteOnAction(
       asin,
@@ -38,22 +52,37 @@ const Cart = () => {
   }, [deleteResponse, response]);
 
   if (fetchErr) {
-    return (
-      <p className="text-3xl text-indigo-500 font-semibold py-5 w-full text-center">
-        Server Error !
-      </p>
-    );
+    return <ServerErr />;
   }
 
   return (
     <>
+      {loading && <Loader message="fetching cart" />}
       <div className="container mx-auto sm:py-8">
-        {loading && <Loader message="fetching cart" />}
         <ToastContainer />
-        {response?.length === 0 && (
+        {response?.length === 0 ? (
           <div className="text-2xl text-indigo-500 text-center font-semibold">
             There is no item in your cart !
           </div>
+        ) : (
+          <>
+            <div className="pb-8">
+              <button
+                className="bg-indigo-500 text-white py-2 px-3 rounded border border-transparent hover:bg-transparent hover:border-indigo-500 duration-200 w-fit flex gap-2 items-center hover:gap-3"
+                onClick={checkoutCart}
+              >
+                <span>Checkout</span>
+                <MdShoppingCartCheckout className="inline" />
+              </button>
+            </div>
+            {showCheckoutForm && (
+              <CheckoutForm
+                asins={asins}
+                back={showCheckoutForm}
+                setBack={setShowCheckoutForm}
+              />
+            )}
+          </>
         )}
         {response?.map((item) => {
           return (
